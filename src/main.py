d6e01514da61208f654eb283064d62dcd975a2b6
Jeppy <carsten.docktor@gmail.com>
2021-02-16 22:07:46 +0100

    ADD user profile link to copyright disclaimer

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # CoinTaxman
-# Copyright (C) 2021  Carsten Docktor
+# Copyright (C) 2021  Carsten Docktor <https://github.com/provinzio>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
